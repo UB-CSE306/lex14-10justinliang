d8dadc617cb9e40ab4dbe85c0bfe774a8c5841fa
10justinliang <10justinliang>
2025-10-22 16:47:11 -0400

    Fixed bugs in the schemeValidator

--- schemeValidator.c
+++ schemeValidator.c
@@ -2,35 +2,46 @@
 
 int schemeValidator(char scheme[]) {
   int result = -1;
-  if (isAlpha(scheme[1])) {
+  if (isAlpha(scheme[0])) {
     result = 0; 
-    int i=2;
-    while (scheme[i++] != '\0') {
-      if (!isAlpha(scheme[i])) result = -1;
-      else if (!isDigit(scheme[i])) result = -1;
-      else if (!isSpecial(scheme[i])) result = -1;
+    int i = 1;
+    while (scheme[i] != '\0') {
+      if (!isAlpha(scheme[i])) {
+        if (!isDigit(scheme[i])) {
+          if (!isSpecial(scheme[i])) {
+            result = -1;
+          }
+        }
+      }
+      i++;
     }
   }
-  if ('A' < scheme[1] && scheme[1] < 'Z') {
+  if ('A' <= scheme[0] && scheme[0] <= 'Z') {
     result = 1;
   }
   return result;
 }
 
 bool isAlpha(char c) {
-  return 'a' < c && c < 'z';
+    if ('a' <= c && c <= 'z') {
+        return true;
+    }
+    else if ('A' <= c && c <= 'Z') {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 bool isSpecial(char c) {
   bool result = false;
   if (c == '+') result = true;
   else if (c == '-') result = true;
-  else result = true;
+  else if (c == '.') result = true; 
   return result;
 }
 
 bool isDigit(char c) {
-  return 0 < c && c < 9;
+  return '0' < c && c < '9';
 }
-
-
